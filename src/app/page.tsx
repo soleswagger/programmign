e2a23bf821550b6797dadfe00ebd1cb1fdab6c40
soleswagger <2121400199@k12.hi.us>
2024-09-24 08:51:33 -1000

    new updates with the database

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import { db } from "~/server/db"
 
 const mockUrls = [
   "https://utfs.io/f/887d212f-4b7e-48e4-8199-62d6c35294ab-m8ejwc.jpg",
@@ -14,10 +15,13 @@ const mockImages = mockUrls.map((url, index)=> ({
   url,
 }));
 
-export default function HomePage() {
+export default async function HomePage() {
+  const posts = await db.query.posts.findMany();
   return (
     <main className="">
       <div className="flex flex-wrap gap-4 brightness-200 " >
+        {posts.map((post) => (
+          <div key={post.id}>{post.name}</div>))}
         {mockImages.map((image) => (
           <div key={image.id} className="w-48">
             <img src={image.url} />
